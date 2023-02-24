7d85b23e1f0c490a6ac14c168420ea09125b16ae
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-02-24 17:05:45 +0100

    Commenti

--- Gioco dei dadi/js/dadi.js
+++ Gioco dei dadi/js/dadi.js
@@ -9,15 +9,19 @@ const diceValues = [1, 2, 3, 4, 5, 6];
 const user = diceValues[Math.floor(Math.random() * diceValues.length)];
 const computer = diceValues[Math.floor(Math.random() * diceValues.length)];
 
-
+//Stampo i valori 
 console.log("user=" + user)
 console.log("computer=" + computer)
+
 //Determino il numero più alto e stampo il messaggio
 
 if (user === computer) {
+//Utente e computer hanno lo stesso valore
     console.log("Pareggio")
 } else if (user < computer){
+//Computer ha un valore più alto
     console.log("Ha vinto il Computer")
 } else {
+//Utente ha un valore più alto
     console.log("Hai vinto!")
 }
